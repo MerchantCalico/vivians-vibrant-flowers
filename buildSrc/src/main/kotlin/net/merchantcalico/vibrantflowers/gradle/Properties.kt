6218a20fb95a40fce3d8e11089a9ae0b354463c9
Anastasia Calico <calico@merchantcalico.net>
2025-06-17 12:04:54 +1000

    docs: Add DoublePi to the mod contributors list.

--- buildSrc/src/main/kotlin/net/merchantcalico/vibrantflowers/gradle/Properties.kt
+++ buildSrc/src/main/kotlin/net/merchantcalico/vibrantflowers/gradle/Properties.kt
@@ -6,7 +6,7 @@ object Properties {
 	const val ARCHIVES_NAME = "vivians-vibrant-flowers"
 	const val MOD_ID = "vibrantflowers"
 	const val MOD_AUTHOR = "MerchantCalico"
-	val MOD_CONTRIBUTORS = listOf("Placeholder")
+	val MOD_CONTRIBUTORS = listOf("DoublePi")
 	const val DESCRIPTION = "A mod that focuses on fleshing out flowers where not present."
 	const val LICENSE = "MPL-2.0"
 
